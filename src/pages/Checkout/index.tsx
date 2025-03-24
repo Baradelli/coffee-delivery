c6fe010545c0e27ed54584f6a3ddc41fbf2dc78a
Vitor Tognetti Baradelli <vitorbaradelli@gmail.com>
2025-03-24 10:40:46 -0300

    Add Checkout page with address and payment forms

--- src/pages/Checkout/index.tsx
+++ src/pages/Checkout/index.tsx
@@ -1,7 +1,102 @@
+import {
+  Bank,
+  CreditCard,
+  CurrencyDollar,
+  MapPinLine,
+  Money,
+} from "phosphor-react";
+import { Container } from "./styles";
+import { defaultTheme } from "../../styles/themes/default";
+import { Input } from "../../components/Input";
+import { Select } from "../../components/Select";
+
 export default function Checkout() {
   return (
-    <div>
-      <h1>Checkout</h1>
-    </div>
+    <Container>
+      <section>
+        <h2>Complete seu pedido</h2>
+
+        <div className="card">
+          <header>
+            <MapPinLine size={22} color={defaultTheme.product["yellow-900"]} />
+
+            <div className="texts">
+              <h3>Endereço de Entrega</h3>
+              <p>Informe o endereço onde deseja receber seu pedido</p>
+            </div>
+          </header>
+
+          <form action="">
+            <div className="zip">
+              <Input id="zip" name="zip" placeholder="CEP" />
+            </div>
+
+            <div className="street">
+              <Input
+                id="street"
+                name="street"
+                placeholder="Rua"
+                className="w-100"
+              />
+            </div>
+
+            <div className="number-complement">
+              <Input id="number" name="number" placeholder="Número" />
+
+              <Input
+                id="complement"
+                name="complement"
+                placeholder="Complemento"
+              />
+            </div>
+
+            <div className="neighborhood-city-state">
+              <Input
+                id="neighborhood"
+                name="neighborhood"
+                placeholder="Bairro"
+              />
+
+              <div className="city-state">
+                <Input id="city" name="city" placeholder="Cidade" />
+
+                <Input
+                  id="state"
+                  name="state"
+                  placeholder="UF"
+                  max={2}
+                  maxLength={2}
+                />
+              </div>
+            </div>
+          </form>
+        </div>
+
+        <div className="card">
+          <header>
+            <CurrencyDollar
+              size={22}
+              color={defaultTheme.product["purple-500"]}
+            />
+
+            <div className="texts">
+              <h3>Pagamento</h3>
+              <p>
+                O pagamento é feito na entrega. Escolha a forma que deseja pagar
+              </p>
+            </div>
+          </header>
+
+          <div className="payment-form-container">
+            <Select icon={<CreditCard />} label="Cartão de crédito" />
+            <Select icon={<Bank />} label="cartão de débito" />
+            <Select icon={<Money />} label="dinheiro" />
+          </div>
+        </div>
+      </section>
+      <section>
+        <h2>Cafés selecionados</h2>
+      </section>
+    </Container>
   );
 }
---
+++ src/pages/Checkout/styles.ts
@@ -0,0 +1,98 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  width: 100%;
+  gap: 2rem;
+
+  section {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+
+    h2 {
+      font-size: ${({ theme }) => theme.size.title.xs};
+      font-weight: ${({ theme }) => theme.weight.bold};
+      color: ${({ theme }) => theme.base.subtitle};
+    }
+
+    .card {
+      display: flex;
+      flex-direction: column;
+      gap: 2rem;
+
+      background-color: ${({ theme }) => theme.base.card};
+      border-radius: 6px;
+      padding: 2.5rem;
+
+      header {
+        display: flex;
+        align-items: flex-start;
+        gap: 0.5rem;
+
+        .texts {
+          h3 {
+            font-size: ${({ theme }) => theme.size.text.m};
+            font-weight: ${({ theme }) => theme.weight.regular};
+            color: ${({ theme }) => theme.base.subtitle};
+          }
+
+          p {
+            font-size: ${({ theme }) => theme.size.text.s};
+            font-weight: ${({ theme }) => theme.weight.regular};
+            color: ${({ theme }) => theme.base.text};
+          }
+        }
+      }
+
+      form {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+
+        .zip {
+          display: grid;
+          grid-template-columns: 1fr 2fr;
+        }
+
+        .street {
+          input {
+            width: 100% !important;
+          }
+        }
+
+        .number-complement {
+          display: grid;
+          grid-template-columns: 1fr 2fr;
+          gap: 0.75rem;
+        }
+
+        .neighborhood-city-state {
+          display: grid;
+          grid-template-columns: 1fr 2fr;
+          gap: 0.75rem;
+
+          .city-state {
+            display: flex;
+            gap: 0.75rem;
+
+            #city {
+              width: 100%;
+            }
+
+            #state {
+              max-width: 60px;
+            }
+          }
+        }
+      }
+
+      .payment-form-container {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr;
+        gap: 0.75rem;
+      }
+    }
+  }
+`;
