64648cf69953c9a0d421c3b5a4c651fccd825c21
Vitor Tognetti Baradelli <vitorbaradelli@gmail.com>
2025-03-24 10:37:14 -0300

    Refactor Select component: change from input to button, add icon and label props, and update styles for selected state

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,13 +1,18 @@
-import { ComponentPropsWithRef } from "react";
+import { ComponentPropsWithRef, ReactNode } from "react";
 
 import { Select as SelectComponent } from "./styles";
 
-type SelectProps = Omit<ComponentPropsWithRef<"input">, "type">;
+type SelectProps = ComponentPropsWithRef<"button"> & {
+  icon: ReactNode;
+  label: string;
+  selected?: boolean;
+};
 
-export const Select = ({ ...rest }: SelectProps) => {
+export const Select = ({ icon, label, selected, ...rest }: SelectProps) => {
   return (
-    <SelectComponent {...rest} type="checkbox">
-      {rest.children}
+    <SelectComponent {...rest} selected={Boolean(selected)}>
+      {icon}
+      {label}
     </SelectComponent>
   );
 };
--- src/components/Select/styles.ts
+++ src/components/Select/styles.ts
@@ -1,17 +1,44 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const Select = styled.input`
+interface SelectProps {
+  selected: boolean;
+}
+
+export const Select = styled.button<SelectProps>`
+  display: flex;
+  align-items: center;
+  gap: 0.75rem;
+
+  text-transform: uppercase;
+  cursor: pointer;
+
   padding: 1rem;
   border-radius: 0.625rem;
 
+  font-size: ${({ theme }) => theme.size.button.m};
+  font-weight: ${({ theme }) => theme.weight.regular};
   color: ${({ theme }) => theme.base.text};
-  font-weight: 400;
+
+  transition: background-color 0.2s;
 
   &:hover {
     background-color: ${({ theme }) => theme.base.hover};
   }
 
+  ${({ selected }) =>
+    selected &&
+    css`
+      outline: 1px solid ${({ theme }) => theme.product["purple-500"]};
+      background-color: ${({ theme }) => theme.product["purple-100"]};
+    `}
+
   &:focus {
     outline: 1px solid ${({ theme }) => theme.product["purple-500"]};
+    background-color: ${({ theme }) => theme.product["purple-100"]};
+  }
+
+  svg {
+    font-size: 16px;
+    color: ${({ theme }) => theme.product["purple-500"]};
   }
 `;
